29351fe62a9ecd11530a8374a042df006ad140e6
jesikatrinitaa <111202214257@mhs.dinus.ac.id>
2024-10-13 17:19:33 +0700

    coba di lokal baru

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function App() { 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h3>Jesika Trinita_</h3>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+	  <>
+	    <h1 className="text-3xl font-bold underline">
+	      ini font body
+	    </h1>
+      <h1 className="text-3xl font-poppins text-primary">
+        ini font poppins
+      </h1>
+      <h1 className="text-3xl">
+        ini font body
+      </h1>
+      <p>
+        ini font body juga. lorem ipsum dolor sit, amet consectetur adipisicing elit. illum perferendis dolores provindent debitis, necessitatibus ullam, quibusdam dolore cumque velit, molestias recusandae exercitationem pariatur. quas ullam aliquid ea. repellat exercitationem similique!
       </p>
-    </>
+	  </>
   )
 }
 
